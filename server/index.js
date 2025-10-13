cce0293f75f2dc607b3c94bf98620db0dfb63087
christinelaics <christinelai.cs@gmail.com>
2025-10-13 17:37:35 -0400

    feat(backend): added proxy routes for top headlines and search

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
+const BASE_URL = "http://newsapi.org/v2";
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,5 +13,37 @@ app.use(express.json());
 app.get("/api/health", (req, res) => {
     res.json({status: "ok", time: new Date().toISOString()});
 })
+
+app.get("/api/top-headlines", async (req, res) => {
+    try {
+        const { category, pageSize=10, page = 1} = req.query;
+
+        const params = {
+            apiKey: NEWS_API_KEY,
+            country: "us",
+            pageSize,
+            page,
+            ...(category && {category})
+        }
+        const response = await axios.get(`${BASE_URL}/top-headlines`, {params});
+        res.json(response.data);
+    } catch (err) {
+        console.log("error fetching headlines: ", err.message);
+        res.status(500).json({error: "failed to fetch top headlines"});
+    }
+});
+
+app.get("/api/search", async (req, res) => {
+    try {
+        const {q, pageSize=20, page=1} = req.query;
+        if(!q) return res.status(400).json({error: "Missing query parameter q"});
+        const response = await axios.get(`${BASE_URL}/everything`, 
+            {params: {apiKey: NEWS_API_KEY, q, pageSize, page}});
+        res.json(response.data);
+    } catch (err) {
+        console.log("error searching news", err.message);
+        res.status(500).json({error: "Failed to search news"});
+    }
+});
 
 app.listen(3000, () => console.log("sever running on http://localhost:3000"));
