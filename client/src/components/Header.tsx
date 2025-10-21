2dd36b31b63cdcfef362a5766a6ccf40712c6b9b
christinelaics <christinelai.cs@gmail.com>
2025-10-21 00:29:19 -0400

    fix(frontend): resolved header issue where it was overlapping with search bar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flexbox justify-center items-center h-[20] border-t-4 border-stone-700 mt-4">
+    <div className="flexbox justify-center items-center h-[20%] border-t-4 border-stone-700 mt-4">
       <hr className="mt-1 border-1 border-stone-700" />
       <a href="/">
         <h1 className="unifrak whitespace-nowrap font-bold text-center text-[clamp(1rem,7.5vw,7rem)]">
